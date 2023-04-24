f5575e04a004bea5387c9ba214b5386e74a083b3
David Bureš <buresdv@gmail.com>
2023-04-24 10:27:54 +0200

    Initial Commit

---
+++ Pali-Go 2/Pali_Go_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pali_Go_2App.swift
+//  Pali-Go 2
+//
+//  Created by David Bureš on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pali_Go_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
